7cfb5f4af2929032f2f6e0afef9a3c43343000da
Vitalii Kovalkovskyi <wnezox@gmail.com>
2024-02-28 18:16:42 +0200

    Add all entities, output of the primary key to the admin panel,name output instead of entity number

--- journal/EduHub/admin.py
+++ journal/EduHub/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import Student
+from .models import Teacher, Student, Course, Enrollment, Grade
 
+
+#це все для відображення первинного ключа
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('teacher_id', 'firstname', 'lastname')
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('stud_id', 'firstname', 'lastname')
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('course_id', 'course_name', 'teacher')
+
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('enrollmer_id', 'student', 'course')
+
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('grade_id', 'student', 'course', 'grade', 'date')
+    
 # Register your models here.
-admin.site.register(Student)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Enrollment, EnrollmentAdmin)
+admin.site.register(Grade, GradeAdmin)
