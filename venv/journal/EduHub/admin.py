3cccb2906e14e2dcc8b859e801bff1f544126206
Roman Pidlipskiy <108235770+Randayy@users.noreply.github.com>
2024-03-02 15:11:50 +0200

    fix

--- venv/journal/EduHub/admin.py
+++ venv/journal/EduHub/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import Teacher, Student, Course, Enrollment, Grade, user
+from .models import Teacher, Student, Course, Enrollment, Grade, User
 
 from django.contrib import admin
 from django.contrib.auth import get_user_model
--- venv/journal/EduHub/models.py
+++ venv/journal/EduHub/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class user(AbstractUser):
+class User(AbstractUser):
   pass
 
 class Teacher(models.Model):
